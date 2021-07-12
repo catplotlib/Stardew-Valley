f4df79fecb4d45f480793c5c7f80413a62677c17
catplotlib <43911345+Puja2402@users.noreply.github.com>
2021-07-12 08:47:48 +0530

    icon changes

--- src/Home.js
+++ src/Home.js
@@ -8,26 +8,29 @@ function Home() {
   return (
     <>
       <img className="Star" src={logo} alt="logo" />
+      {/* Laptop */}
       <div className="row desk">
         <div className="column">
           <img src={abigail} className="abi" alt="logo" />
         </div>
-      
-        <div className="column desk">
-        <a href='https://store.steampowered.com/app/413150/Stardew_Valley/'>
-          <img src={steam} className="steam" alt="logo" />
+
+        <div className="column">
+          <a href="https://store.steampowered.com/app/413150/Stardew_Valley/">
+            <img src={steam} className="steam" alt="logo" />
           </a>
         </div>
-       
+
         <div className="column"></div>
         <img src={el} className="el" alt="logo" />
       </div>
 
+      {/* mobile */}
+
       <div className="div-only-mobile">
-      <div className="row">
-        <div className="column">
-          <img src={steam} className="steam" alt="logo" />
-        </div>
+        <div className="row">
+          <div className="column">
+            <img src={steam} className="steam" alt="logo" />
+          </div>
         </div>
         <div className="row">
           <div className="column">
